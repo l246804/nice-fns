d01e47071c293c18ddbeb9c4d27620770651aaa7
Ray Hao <2468048176@qq.com>
2024-08-01 11:00:53 +0800

    feat: 🎸 add isPromise, isPromiseLike

---
+++ src/isPromiseLike.ts
@@ -0,0 +1,10 @@
+import { isFunction } from 'lodash-unified'
+import { isPromise } from './isPromise'
+
+/**
+ * 检测值是否支持 `then()` 回调
+ * @param val 待检测值
+ */
+export function isPromiseLike(val: any): val is PromiseLike<any> {
+  return val != null && (isPromise(val) || isFunction(val.then))
+}
